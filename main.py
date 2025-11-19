9dd0acad0bbf172a7160b8fb764dda5ee986de11
sybyeon1102 <sybyeon1102@gmail.com>
2025-11-19 15:41:58 +0900

    conflict solved

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 print("hello")
 
-<<<<<<< HEAD
+print("main")
+
 for i in range(5):
     for j in range(7):
         print(i*j)
-<<<<<<< HEAD
-=======
-print("main")
->>>>>>> fecb8ab (conflict)
-=======
-
-print("commit 안 한 것은 날아가나요?")
->>>>>>> 0683e95 (conflict test)
